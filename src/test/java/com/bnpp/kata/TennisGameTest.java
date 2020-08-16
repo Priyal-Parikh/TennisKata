f0db70f45d4555767adff26e15872e3d99e0a9d4
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-16 16:33:10 +0530

    Refactor: Initialize TennisGame in a different setup method

--- src/test/java/com/bnpp/kata/TennisGameTest.java
+++ src/test/java/com/bnpp/kata/TennisGameTest.java
@@ -1,26 +1,29 @@
 package com.bnpp.kata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest
 {
-
     public static final String PLAYER_ONE_NAME = "Serena Williams";
     public static final String PLAYER_TWO_NAME = "Maria Sharapova";
     public static final int POINT_ONE = 1;
+    TennisGame tennisGame;
+
+    @Before
+    public void setUp() {
+        tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
+    }
 
     @Test
     public void shouldInitializeNewTennisGameWithTwoPlayers() {
-        TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
-
         Assert.assertEquals(PLAYER_ONE_NAME, tennisGame.getPlayerOneName());
         Assert.assertEquals(PLAYER_TWO_NAME, tennisGame.getPlayerTwoName());
     }
 
     @Test
     public void playerOneScoreIncreasesAfterWinningAPoint() {
-        TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
         tennisGame.playerOneScoresPoint();
 
         Assert.assertEquals(POINT_ONE, tennisGame.getPlayerOnePoint());
@@ -28,7 +31,6 @@ public class TennisGameTest
 
     @Test
     public void playerTwoScoreIncreasesAfterWinningAPoint() {
-        TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
         tennisGame.playerTwoScoresPoint();
 
         Assert.assertEquals(POINT_ONE, tennisGame.getPlayerTwoPoint());
